9119f786ba9a1632cbb25899d761053844c3f283
ABlam87 <adamblampied@gmail.com>
2024-01-12 17:40:27 +0000

    README Structure written, inquirer test successful

---
+++ starter/READMESample.md
@@ -0,0 +1,11 @@
+
+# Sample README
+
+## Description
+
+This is a quick sample to make sure it works
+
+## Table of Contents
+
+tbc
+      
--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,7 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+   
 ];
 
 // function to write README file
@@ -14,8 +14,78 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    
 
 }
 
 // function call to initialize program
-init();
+// init();
+
+inquirer
+    .prompt([
+        {type: 'input',
+        name: 'title',
+        message: "What is the title of your project?"}
+    ,
+        {type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project'}
+    ])
+    
+    .then((answers) => {
+
+      const READMEfile = `
+# ${answers.title}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- Description
+- Installation
+- Usage
+- License
+- Contributors
+- Test Instructions
+- Contact
+
+## Installation
+
+
+
+## Usage
+
+
+
+## License
+
+
+
+## Contributors
+
+
+
+## Test Instructions
+
+
+
+## Contact
+
+For any further questions, please email:
+
+
+      `
+      fs.writeFile('READMESample.md', READMEfile, (err) => 
+      err? console.error(err) : console.log('success!'));
+
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error;
+      } else {
+        console.log('Thank you!')
+      }
+    });
+
